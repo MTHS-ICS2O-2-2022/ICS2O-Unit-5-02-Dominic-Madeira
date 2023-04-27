a64a0b1c28f7243b26f3632a316cefe8561b8f19
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-04-27 17:28:11 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+function myButtonClicked() {
+  const positiveButton = document.getElementById("on-check").checked
+
+  if (positiveButton == true) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNumber
+  } else {
+    const randomNegativeNumber = Math.floor(Math.random() * 6 + 1) * -1
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNegativeNumber
+  }
+}
